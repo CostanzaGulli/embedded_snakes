0968b85b56381bfefb34899e753d0ce492a929af
CostanzaGulli <gullicostanza98@gmail.com>
2020-01-26 14:22:33 +0000

    update

--- HTTP/http_example.py
+++ HTTP/http_example.py
@@ -1,7 +1,8 @@
 import requests 
   
 # api-endpoint 
-URL = "http://44ac3734.ngrok.io"
+# URL = "http://44ac3734.ngrok.io"
+URL = "http://9f5767b3.ngrok.io"
     
 # sending get request and saving the response as response object 
 r = requests.post(url = URL, params = {"1":2}) 
@@ -12,4 +13,4 @@ data = r.json()
   
 # printing the output 
 print("Latitude:%s\nLongitude:%s\nFormatted Address:%s"
-      %(latitude, longitude,formatted_address)) 
+      %(latitude, longitude,formatted_address) ) 
