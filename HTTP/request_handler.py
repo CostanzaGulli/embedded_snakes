f776e9623ef78e41f3483cd2f7af1216f1daa5a7
EirikAlbrektsen <52484057+EirikAlbrektsen@users.noreply.github.com>
2020-01-26 13:10:53 +0000

    Create request_handler.py
    Added skeleton of methods for posting, getting and head

---
+++ HTTP/request_handler.py
@@ -0,0 +1,16 @@
+import http.server
+import socketserver
+
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
+    def do_GET(self):
+        """Respond to a GET request."""
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
+    def do_POST(self):
+        """Respond to a POST request"""
+        self.send_response(200)
+        self.end_headers()
