57b42d8e8fc2be756d649faba3bbb62aab513dce
EirikAlbrektsen <52484057+EirikAlbrektsen@users.noreply.github.com>
2020-02-06 11:53:46 +0000

    Update publisher_example.py
    Update  template for data being published

--- firestoreMqttClient/publisher_example.py
+++ firestoreMqttClient/publisher_example.py
@@ -6,12 +6,23 @@ import client
 client.mqtt_connect()
 
 
-data = {
-    "move":"Shake",
-    "time":1.4,
-    "user":"Eirik",
-    "success":True
+data_move = {
+        "type":"moves", 
+        "data":{
+            "move":"Shake",
+            "time":1.4,
+            "user":"Eirik",
+            "success":True
+        }
+}
+data_game = {
+        "type":"games", 
+        "data":{
+            "win":True,
+            "score":14000,
+            "user":"Eirik",
+        }
 }
 while 1:
     time.sleep(5)
-    client.mqtt_publish("IC.embedded/snakes/test",str(json.dumps(data)))
+    client.mqtt_publish("IC.embedded/snakes/test",str(json.dumps(data_game)))
