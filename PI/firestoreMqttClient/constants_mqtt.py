c7baf64d3a97f7e592d29d928d5232f83100c9af
CostanzaGulli <gullicostanza98@gmail.com>
2020-02-11 10:50:43 +0000

    main update for 2 players

--- PI/firestoreMqttClient/constants_mqtt.py
+++ PI/firestoreMqttClient/constants_mqtt.py
@@ -1,2 +1,3 @@
 broker = "test.mosquitto.org"
 path = "IC.embedded/snakes/test"
+path_startgame = "IC.embedded/snakes/startgame"
--- PI/main.py
+++ PI/main.py
@@ -4,6 +4,9 @@ import RPi.GPIO as GPIO
 import start
 import game1
 
+import firestoreMqttClient.client as cl
+import firestoreMqttClient.constants_mqtt as const
+
 start.start() #set up mqtt connection and input pins
 
 #while true . program keeps running
@@ -23,9 +26,12 @@ while start == False:
         time.sleep(0.1)
         elapsed_start_time = time.time() - start_start_time
     if (not_pressed_once == True) and elapsed_start_time > 1:
+        start=True
         print("GAME 1")
         game1.game()
-        start=True
     if (not_pressed_once == True) and (input_state == True) and (elapsed_start_time<=1):
-        print("GAME 2")
         start=True
+        print("GAME 2")
+
+        # Send message to Pi1 to start the game.
+        cl.mqtt_publish(const.path_startgame, "START")
