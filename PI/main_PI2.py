90a45753f136ecbb9aae9fb78ac7ff31d7ed441f
vc1917 <60190524+vc1917@users.noreply.github.com>
2020-02-13 10:45:55 +0000

    last changes

--- PI/main_PI2.py
+++ PI/main_PI2.py
@@ -1,33 +1,27 @@
 import time 
 import RPi.GPIO as GPIO
-
 import start
 import game1
-
 import firestoreMqttClient.constants_mqtt as constants_mqtt
-
 import paho.mqtt.client as mqtt
 import paho.mqtt.subscribe as sub
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected with result code: "+str(rc))
 
 def on_message(client, userdata, msg):
-    print("ON_MESSAGE"+msg.topic+" "+str(msg.payload))
+    print("ON_MESSAGE: "+msg.topic+" "+str(msg.payload))
 
 def on_message_print(client, userdata, message):
-    print("ONMESSAGE PRINT"+"%s e anche %s" % (message.topic, message.payload))
-    if message.payload.decode() == "START":
-        #print("YESSS LETS START")
-        start=True
+    print("ON_MESSAGE PRINT"+"%s e anche %s" % (message.topic, message.payload))
+    if message.payload.decode() == "START": # if receive START message from Pi1, start the game
+        start = True
         game1.game()
         print("ESCO?")
-        client.unsubscribe(constants_mqtt.path_startgame)
-        print("e ora come la mettiamo?")
 
 client = mqtt.Client()
 def mqtt_connect():  
-    return_code = client.connect(constants_mqtt.broker, 8884)#POI FA ENCRIPTION
+    return_code = client.connect(constants_mqtt.broker, 8884) # using encrypted channel
     client.on_connect = on_connect
     if return_code == 0:
         print("connection succesful\n")
@@ -35,19 +29,13 @@ def mqtt_connect():
         error_string = mqtt.error_string(return_code)
         print("the error is "+error_string+" code "+return_code+'\n')
 
-#def mqtt_publish(path, mqtt_message):
-#    msg_info = client.publish(path,mqtt_message)
-#    publish_out = mqtt.error_string(msg_info.rc)
-#    print("publish status: "+publish_out+"\n")
-
 def mqtt_subscribe(host_name, path):
     client.on_message = on_message
-    client.loop(1)
-    sub.callback(on_message_print, path, hostname=host_name)
+    sub.callback(on_message_print, path, hostname = host_name)
 
-start.start() #set up mqtt connection and input pins
+start.start() # call function to set up mqtt connection and input pins
 
 start = False
-while start == False:
+while start == False: # wait for START message to be received 
     mqtt_subscribe(constants_mqtt.broker, constants_mqtt.path_startgame)
 print("esco2?")
